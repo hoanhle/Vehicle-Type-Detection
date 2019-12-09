534d3a872a7c84e99d1dd48f81804ef8220f82dc
Hoanh Le <hoanh.le@tuni.fi>
2019-12-09 20:50:44 +0200

    Implement merge models together

--- test_trained_CNN.py
+++ test_trained_CNN.py
@@ -6,22 +6,36 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 from combine_model import ensemble_predictions
 
+
 # CHANGE THESE
 all_dir = 'train/train'
 test_dir = "testset/"
+model_path = 'my_model.h5'
 
 # Create an index of class names
 
 class_names = sorted(os.listdir(all_dir))
 
-
+trained_model = load_model(model_path)
 """
-TODO: Change model paths
+TODO: Saved inception model to h5 and load it here
 """
 model1 = load_model("my_model.h5")
-model2 = load('./InceptionV3-model')
-
+model2 = load_model("inception_model.h5")
 models = [model1, model2]
+
+def ensemble_models(models):
+    model_input = tf.keras.Input(shape = models[0].input_shape[1:])
+    outModels = [model.outputs[0] for model in models]
+    avgLay = tf.keras.layers.Average()(outModels)
+
+    # Merge into one model
+
+    modelMerge = tf.keras.Model(inputs = model_input, outputs = avgLay, name='ensemble')
+
+    return modelMerge
+
+modelMerge = ensemble_models(models)
 
 
 with open("submissionCNN.csv", "w") as fp:
@@ -31,7 +45,6 @@ with open("submissionCNN.csv", "w") as fp:
     i = 0
     # 1. load image and resize
     for file in os.listdir(test_dir):
-        print(file)
         if file.endswith(".jpg"):
             # Load the image
             img = plt.imread(test_dir + file)
@@ -44,7 +57,7 @@ with open("submissionCNN.csv", "w") as fp:
 
             # Predict class by picking the highest probability index
             # then add 1 (due to indexing behavior)
-            class_index = ensemble_predictions(models, img)
+            class_index = np.argmax(modelMerge.predict(img)[0])
 
             # Convert class id to name
             label = class_names[class_index]
