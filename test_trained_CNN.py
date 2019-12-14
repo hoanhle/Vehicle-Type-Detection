1556bca439d8649b94641a9eb605d221a0ff10ef
Hoanh Le <hoanh.le@tuni.fi>
2019-12-14 15:59:19 +0200

    Add models for ensembling

--- test_trained_CNN.py
+++ test_trained_CNN.py
@@ -9,36 +9,32 @@ from tensorflow.keras.models import load_model
 # CHANGE THESE
 all_dir = 'train/train'
 test_dir = "testset/"
-model_path = 'my_model.h5'
 
 # Create an index of class names
 
 class_names = sorted(os.listdir(all_dir))
 
-trained_model = load_model(model_path)
 """
 TODO: Saved inception model to h5 and load it here
 """
-model1 = load_model("my_model.h5")
-model2 = tf.saved_model.load('./InceptionV3-model')
+model1 = load_model('mobilenet2.h5')
+model2 = load_model('InceptionV33.h5')
+model3 = load_model('densenet.h5')
 
-models = [model1, model2]
+models = [model1, model2, model3]
 
-def ensemble_models(models):
-    model_input = tf.keras.Input(shape = models[0].input_shape[1:])
-    outModels = [model.outputs[0] for model in models]
-    avgLay = tf.keras.layers.Average()(outModels)
-
-    # Merge into one model
-
-    modelMerge = tf.keras.Model(inputs = model_input, outputs = avgLay, name='ensemble')
-
-    return modelMerge
-
-modelMerge = ensemble_models(models)
+def ensemble_predictions(members, testX):
+    	# make predictions
+	yhats = [model.predict(testX) for model in members]
+	yhats = np.array(yhats)
+	# sum across ensemble members
+	summed = np.sum(yhats, axis=0)
+	# argmax across classes
+	result = np.argmax(summed, axis=1)
+	return result
 
 
-with open("submissionCNN.csv", "w") as fp:
+with open("submissionEnsemble3.csv", "w") as fp:
     fp.write("Id,Category\n")
 
     # Image index
@@ -57,8 +53,7 @@ with open("submissionCNN.csv", "w") as fp:
 
             # Predict class by picking the highest probability index
             # then add 1 (due to indexing behavior)
-            class_index = np.argmax(modelMerge.predict(img)[0])
-            class_index = ensemble(models, img)
+            class_index = ensemble_predictions(models, img)[0]
 
             # Convert class id to name
             label = class_names[class_index]
